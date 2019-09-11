7de3d7fc40e7acda359809d983803bdf8c7df9c9
Brandon Lee <yongho19911101@hotmail.com>
2019-09-11 16:57:08 -0700

    readme added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from "react";
+
+const initialState = {
+  toDos: []
+}
+const ADD = "add";
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case ADD:
+      return { toDos: [...state.toDos, {text: action.payload}] };
+    default:
+      throw new Error();
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [newToDo, setNewToDo] = useState();
+  const onSubmit = e => {
+    e.preventDefault();
+    dispatch({ type: ADD, payload: newToDo })
+  }
+  const onChange = e => {
+    const {
+      target: { value }
+    } = e;
+    setNewToDo(value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <h1>Add To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          type="text" 
+          placeholder="Write To Do"
+          value={newToDo}
+          onChange={onChange}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </input>
+      </form>
+      <ul>
+        <h3>To Dos</h3>
+        {state.toDos.map((toDo, index) => <li key={index}>{toDo.text}</li> )}
+      </ul>
+    </>
+  )
 }
 
 export default App;
+
+
+// import React, {useState} from "react";
+// import Screen from "./Screen";
+// import Lang from "./context";
+// import translations from "./translations"
+// function App() {
+//   return (
+//     <Lang defaultLang="en" translations={translations}>
+//       <Screen />
+//     </Lang>
+//   )
+// }
+
+// export default App;
+
---
+++ src/Screen.js
@@ -0,0 +1,17 @@
+// import React, { Component} from "react";
+// import {useSetLang, useT} from "./context";
+
+// export default () => {
+//     const setLang = useSetLang();
+//     const t = useT();
+//     return (
+//     <>
+//         <h1>
+//             {t("Hello")}
+//         </h1>
+//         <button onClick={() => setLang("es")}>
+//             {t("Translate")}
+//         </button>
+//     </>
+//     )
+// };
